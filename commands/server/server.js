9d9f496f1fa0d60d55eeaafb8c4ccf25ddaf0915
Shadav <55554713+leashadav@users.noreply.github.com>
2025-01-13 05:45:26 -0500

    Add files via upload

--- commands/server/server.js
+++ commands/server/server.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setDescription('Info about the server.'),
 	async execute(interaction) {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nDescription: ${interaction.guild.description}\nCreated on: <t:${Math.round(interaction.guild.createdTimestamp / 1000)}>\nTotal members: ${interaction.guild.memberCount}\nBoost level: ${interaction.guild.premiumTier}\nTotal boosts: ${interaction.guild.premiumSubscriptionCount || '0'}\nServer owner: <@!${interaction.guild.ownerId}>`);
 	},
