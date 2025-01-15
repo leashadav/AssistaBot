8da15b7bec253cb55fd07f72ede0333928dff83a
Shadav <55554713+leashadav@users.noreply.github.com>
2025-01-15 00:04:42 -0500

    Add files via upload

--- delete-commands.js
+++ delete-commands.js
@@ -1,10 +1,7 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
-
 const rest = new REST({ version: '9' }).setToken(token);
-
-// ...
 
 // for guild-based commands
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
