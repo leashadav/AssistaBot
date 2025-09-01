ef56caf380b2d407a0b0e251556abed5341eaa21
Shadav <55554713+leashadav@users.noreply.github.com>
2025-08-31 20:22:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Main entry point � loads handlers, logs in client
 const { Client, Collection, GatewayIntentBits, Partials, Events, ActivityType, PresenceUpdateStatus, MessageFlags } = require('discord.js');
 const fs = require('fs');
-const { prefix, token } = require('./config.json');
+const { prefix, DISCORD_TOKEN } = require('./config.json');
 require('dotenv').config();
 
 const client = new Client({
@@ -35,5 +35,6 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args, client));
   }
 }
+
 
 client.login(process.env.DISCORD_TOKEN);
